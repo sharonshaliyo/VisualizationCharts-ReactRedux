118041207aa6c7922516054f6c9f7ad250f135a4
Sharon Shaliyo <sharon.shaliyo@gmail.com>
2023-10-02 04:10:19 +0530

    feat - render to other element

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import renderHighChart from './components/piechart/index'
-import { useLayoutEffect } from 'react';
+import Piechart from './components/piechart/index'
 
 function App() {
-  useLayoutEffect(() => {
-    renderHighChart()
-  })
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p> */}
-        <div id="container"></div>
+        <Piechart />
       </header>
     </div>
   );
--- src/components/piechart/index.js
+++ src/components/piechart/index.js
@@ -1,14 +1,29 @@
+import React, { useRef, useLayoutEffect } from 'react'
 import Highcharts from 'highcharts'
+
 import options from './options'
 import buildTooltipOptions from './options/tooltipOptions'
 import buildLegendOptions from './options/legendOptions'
 
-const renderHighChart = () => {
-    Highcharts.chart('container', {
+const renderHighChartRef = (element) => {
+    Highcharts.chart({
         ...options,
+        chart: {
+            ...options.chart,
+            renderTo: element
+        },
         tooltip: buildTooltipOptions(),
         legend: buildLegendOptions()
     })
 }
 
-export default renderHighChart
+const Piechart = () => {
+    const containerRef = useRef(null)
+    useLayoutEffect(() => {
+        renderHighChartRef(containerRef.current)
+    })
+
+    return <div ref={containerRef}></div>
+}
+
+export default Piechart
